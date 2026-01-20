904651a6f07bff3c150a9698bcdafea44e2d676a
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-01-20 22:11:24 +0300

    feat(sdk): add cluster status support and models to Go SDK

--- pkg/sdk/storage.go
+++ pkg/sdk/storage.go
@@ -18,6 +18,25 @@ type Object struct {
 	CreatedAt   time.Time `json:"created_at"`
 }
 
+// Bucket describes a storage bucket.
+type Bucket struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	IsPublic  bool      `json:"is_public"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type StorageNode struct {
+	ID       string    `json:"id"`
+	Address  string    `json:"address"`
+	Status   string    `json:"status"`
+	LastSeen time.Time `json:"last_seen"`
+}
+
+type StorageCluster struct {
+	Nodes []StorageNode `json:"nodes"`
+}
+
 func (c *Client) ListObjects(bucket string) ([]Object, error) {
 	var res Response[[]Object]
 	if err := c.get(fmt.Sprintf("/storage/%s", bucket), &res); err != nil {
@@ -57,4 +76,39 @@ func (c *Client) DownloadObject(bucket, key string) (io.ReadCloser, error) {
 
 func (c *Client) DeleteObject(bucket, key string) error {
 	return c.delete(fmt.Sprintf("/storage/%s/%s", bucket, key), nil)
+}
+
+func (c *Client) CreateBucket(name string, isPublic bool) (*Bucket, error) {
+	req := struct {
+		Name     string `json:"name"`
+		IsPublic bool   `json:"is_public"`
+	}{
+		Name:     name,
+		IsPublic: isPublic,
+	}
+	var res Response[Bucket]
+	if err := c.post("/storage/buckets", req, &res); err != nil {
+		return nil, err
+	}
+	return &res.Data, nil
+}
+
+func (c *Client) ListBuckets() ([]Bucket, error) {
+	var res Response[[]Bucket]
+	if err := c.get("/storage/buckets", &res); err != nil {
+		return nil, err
+	}
+	return res.Data, nil
+}
+
+func (c *Client) DeleteBucket(name string) error {
+	return c.delete(fmt.Sprintf("/storage/buckets/%s", name), nil)
+}
+
+func (c *Client) GetStorageClusterStatus() (*StorageCluster, error) {
+	var res Response[StorageCluster]
+	if err := c.get("/storage/cluster/status", &res); err != nil {
+		return nil, err
+	}
+	return &res.Data, nil
 }
