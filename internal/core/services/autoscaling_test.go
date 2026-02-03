9879d9e6af4b924d1d8f99cd6a0d90f85bade5a7
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-03 18:18:13 +0300

    test: update autoscaling tests for interface changes

--- internal/core/services/autoscaling_test.go
+++ internal/core/services/autoscaling_test.go
@@ -237,7 +237,7 @@ func TestAutoScaling_TriggerScaleUp(t *testing.T) {
 
 	// Wait for instance to be running (provision job)
 	// We manually provision it since we don't have the task worker running
-	err = instSvc.Provision(ctx, instID, nil)
+	err = instSvc.Provision(ctx, instID, nil, "")
 	require.NoError(t, err)
 
 	// 5. Create Scaling Policy (CPU > 50%)
