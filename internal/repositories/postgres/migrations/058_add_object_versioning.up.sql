117116daa8da69ed63f5ca1321692381f25d12b2
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-04 13:40:16 +0300

    fix(migrations): drop old object unique constraint
    Ensure 'objects_bucket_key_key' is dropped when adding versioning support to prevent constraint violations on multiple versions.

--- internal/repositories/postgres/migrations/058_add_object_versioning.up.sql
+++ internal/repositories/postgres/migrations/058_add_object_versioning.up.sql
@@ -8,6 +8,7 @@ UPDATE objects SET version_id = 'null' WHERE version_id IS NULL;
 
 -- Remove old unique constraint and add new one that includes version_id
 -- Note: Constraint name might vary, but objects_bucket_key_key is standard for (bucket, key)
+ALTER TABLE objects DROP CONSTRAINT IF EXISTS objects_bucket_key_key;
 ALTER TABLE objects DROP CONSTRAINT IF EXISTS objects_bucket_key_version_unique;
 ALTER TABLE objects ADD CONSTRAINT objects_bucket_key_version_unique UNIQUE (bucket, key, version_id);
 
