ad34d6f35846d4c4a5f1e51a4b8b10a81867182b
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-03-02 16:41:00 +0300

    feat(ccm): add cloud-controller-manager entrypoint binary

---
+++ cmd/ccm/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	"k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/options"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/logs"
+	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
+	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
+
+	"github.com/spf13/pflag"
+
+	_ "github.com/poyrazk/thecloud/internal/ccm" // Register the cloud provider
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	ccmOptions, err := options.NewCloudControllerManagerOptions()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to initialize command options: %v\n", err)
+		os.Exit(1)
+	}
+
+	command := app.NewCloudControllerManagerCommand(
+		ccmOptions,
+		cloudInitializer,
+		app.DefaultInitFuncConstructors,
+		map[string]string{},
+		cliflag.NamedFlagSets{},
+		wait.NeverStop,
+	)
+
+	// Explicitly add Go flags to the command (for klog)
+	pflag.CommandLine.AddGoFlagSet(nil)
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func cloudInitializer(config *config.CompletedConfig) cloudprovider.Interface {
+	cloudConfig := config.ComponentConfig.KubeCloudShared.CloudProvider
+
+	// Initialize the cloud provider
+	cloud, err := cloudprovider.InitCloudProvider(cloudConfig.Name, cloudConfig.CloudConfigFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cloud provider could not be initialized: %v\n", err)
+		os.Exit(1)
+	}
+	if cloud == nil {
+		fmt.Fprintf(os.Stderr, "Cloud provider is nil\n")
+		os.Exit(1)
+	}
+
+	if !cloud.HasClusterID() {
+		if config.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+			fmt.Println("warning: cluster ID is not set. Many cloud providers require this.")
+		} else {
+			fmt.Fprintf(os.Stderr, "error: cluster ID is required but not set\n")
+			os.Exit(1)
+		}
+	}
+
+	return cloud
+}
