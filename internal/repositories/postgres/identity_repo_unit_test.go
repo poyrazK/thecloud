8c89547ab28b5720585b7e09b5ced432db5406cf
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-03-15 16:03:08 +0300

    fix: remove unused appcontext import in identity repo unit test

--- internal/repositories/postgres/identity_repo_unit_test.go
+++ internal/repositories/postgres/identity_repo_unit_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/pashagolub/pgxmock/v3"
-	appcontext "github.com/poyrazk/thecloud/internal/core/context"
 	"github.com/poyrazk/thecloud/internal/core/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
